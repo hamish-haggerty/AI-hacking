02addd64d92335c61b681a983fee63143f08781f
Hamish Haggerty <hamish.haggerty@uqconnect.edu.au>
2022-07-14 13:04:43 +1000

    Mostly just changed setup.py in SVGD to try and get it working...

--- SVGD/SVGD_packages/setup.py
+++ SVGD/SVGD_packages/setup.py
@@ -4,4 +4,5 @@ setup(
 name='Base_Stein',
 packages=find_packages(where='src'),
 package_dir={'': 'src'},
+url='https://github.com/hamish-haggerty/AI-hacking/tree/master/SVGD/SVGD_packages' #comment this out /delete if it doesn't work...
 )
